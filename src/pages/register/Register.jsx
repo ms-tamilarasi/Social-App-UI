45d38d10c78aba4ac9ad903003e658f271a81398
Tamilarasi <tamil101520@gmail.com>
2024-02-15 10:22:52 +0530

    main

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Social Network</h1>
+          <p>
+          Connect with people who knows you and who doesn't knows you and also everybody you want.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
